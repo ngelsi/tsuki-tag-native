a2841a0c0129ab002398b8bcdea53d1bc53c2dd3
Norbert Gelsi <norbert.gelsi@gmail.com>
2023-02-27 10:23:02 +0100

    .NET major version and assembly version bump.

--- TsukiTag/Dependencies/OnlinePictureProviderElement.cs
+++ TsukiTag/Dependencies/OnlinePictureProviderElement.cs
@@ -5,7 +5,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using TsukiTag.Extensions;
 using TsukiTag.Models;
 
 namespace TsukiTag.Dependencies
